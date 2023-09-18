72783daba69b15273e65ecff8ee4feae8cbc00a3
Saurabhgawade <gawadesaurabh26@gmail.com>
2023-09-18 12:46:13 +0530

    vehicle change 1

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -11,14 +11,47 @@ public class Car extends Vehicle {
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.wheels=wheels;
+        this.doors=doors;
+        this.isManual=isManual;
+        this.type=type;
+        this.seats=seats;
+        this.gears=gears;
+
+        currentGear=1;
     }
 
+    public int getWheels(){
+        return wheels;
+    }
+    public int getDoors(){
+        return doors;
+
+    }
+    public boolean isManual(){
+        return isManual;
+    }
+    public String getType(){
+        return type;
+    }
+    public int getSeats(){
+        return seats;
+    }
+    public int getGears(){
+        return gears;
+    }
+    public int getCurrentGear(){
+        return currentGear;
+    }
     public void changeGear(int newGear){
+        currentGear=newGear;
 
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
+        move(newSpeed,newDirection);
 
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
---
+++ src/test/java/com/driver/WaterVehicleTest.java
@@ -0,0 +1,16 @@
+package com.driver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WaterVehicleTest {
+
+    @Test
+    void getVehicleName() {
+    }
+
+    @Test
+    void getVehicleCapacity() {
+    }
+}
